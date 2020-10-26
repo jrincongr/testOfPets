2aaf4628e1eb8e1160b66d620a2773afe82027a7
javier <52806801+jrincongr@users.noreply.github.com>
2020-10-26 15:35:42 -0600

    Introduced the questions and answers arrays

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,28 @@ var opt4 = document.getElementById("option4")
 
 var currentQuestion=0;
 var countdown = 10;
+
+QnA = [
+    {
+        question: "What is the best animal?",
+        options: ["dogs", "cats", "camels", "lizards"],
+        crcAnswer: "dogs"
+    },
+    {
+        question:"What is the worst animal",
+        options:["dogs", "cats", "camels", "lizards"],
+        crcAnswer: "cats"
+    },
+    {
+        question: "What is the weirdest animal?",
+        options: ["dogs", "cats", "camels", "lizards"],
+        crcAnswer: "camels"
+    },
+    {
+        question: "What is the most amazing animal?",
+        options: ["dogs", "cats", "camels", "lizards"],
+        crcAnswer: "lizards"
+    },
+
+
+]
