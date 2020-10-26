bb932580edbb1a092ce2fcf9d06cdd7cfbf5c6c9
javier <52806801+jrincongr@users.noreply.github.com>
2020-10-26 15:29:08 -0600

    added variables into js

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+var btn = document.getElementById("test-button");
+var headerHtml = document.getElementById("testingVisual");
+var questionArea = document.getElementById("questionArea")
+var questionEl = document.getElementById("question");
+
+//buttons
+var opt1 = document.getElementById("option1")
+var opt2 = document.getElementById("option2")
+var opt3 = document.getElementById("option3")
+var opt4 = document.getElementById("option4")
+
+var currentQuestion=0;
+var countdown = 10;
